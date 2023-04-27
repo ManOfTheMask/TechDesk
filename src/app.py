55d1a1e256c0c9638086dbc89c649b0f861a4543
Dr.Mask <ecdthegreat@gmail.com>
2023-04-27 12:44:54 -0400

    fixed sidebar and added signup page

--- src/app.py
+++ src/app.py
@@ -7,7 +7,12 @@ def index():
     return render_template('index.html')
 
 @app.route("/login")
-def child():
+def login():
     return render_template('login.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
